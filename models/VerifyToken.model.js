272c02d73fc8958b32d022e80c9c7030c8f32ce7
Vishesh <s223535784@deakin.edu.au>
2023-10-03 10:56:57 +0530

    changes in token model

--- models/VerifyToken.model.js
+++ models/VerifyToken.model.js
@@ -2,29 +2,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const verifyTokenSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "users",
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["verify-email", "forget-password", "oAuthRequest", "localToOAuth"],
-  },
-  createdAt: {
-    type: Date,
-    expires: "7d",
-    default: Date.now,
-  },
-});
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["verify-email", "forget-password", "oAuthRequest", "localToOAuth"],
+    },
+    createdAt: {
+      type: Date,
+      expires: "7d",
+      default: Date.now,
+    },
+  });
 
 module.exports = mongoose.model(
-  "verifyToken",
-  verifyTokenSchema,
-  "verifyTokens"
+    "verifyToken",
+    verifyTokenSchema,
+    "verifyTokens"
 );
