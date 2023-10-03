ccca59313c6296d4b5e6622b6ddb9558c02802a2
Vishesh <s223535784@deakin.edu.au>
2023-10-03 12:37:58 +0530

    commit new changes

--- models/PostGift.model.js
+++ models/PostGift.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// create a schema
+
 const PostGiftSchema = new Schema(
   {
     gift: { type: Schema.Types.ObjectId, ref: "Gift", required: true },
