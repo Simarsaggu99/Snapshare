a5604dccdec1de48b06cd9de36d92acac373bb20
Vishesh <s223535784@deakin.edu.au>
2023-10-03 12:28:01 +0530

    post model changes

--- models/Post.model.js
+++ models/Post.model.js
@@ -31,7 +31,7 @@ const postSchema = new Schema(
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
   }
 );
-// the schema is useless so far
+
 // we need to create a model using it
 const Post = model("Post", postSchema, "post");
 
