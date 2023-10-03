5080641ba8258fb8ba279b6acf0ea7f1eae92988
Vishesh <s223535784@deakin.edu.au>
2023-10-03 10:57:34 +0530

    changes in wallet model

--- models/Wallet.model.js
+++ models/Wallet.model.js
@@ -3,21 +3,19 @@ const Schema = mongoose.Schema;
 // create a schema
 const WalletSchema = new Schema(
   {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      unique: true,
-    },
-    meme_coins: { type: Number, required: true, default: 0 },
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        unique: true,
+      },
+      meme_coins: { type: Number, required: true, default: 0 },
   },
   {
-    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+      timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
   }
 );
 
-// we need to create a model using it
 const Wallet = mongoose.model("Wallet", WalletSchema, "wallet");
 
-// make this available to our users in our Node applications
 module.exports = Wallet;
